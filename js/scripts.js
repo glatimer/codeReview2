7efe5f831b39872ef788a2078e060b0577841d9a
Genevieve Latimer <genevievelatimer@gmail.com>
2016-02-19 15:43:35 -0800

    Change column size for q&a display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,54 @@
 $(document).ready(function() {
-  
+  $("#questions").submit() {
+    var question1 = $("button#goal").val();
+    var question2 = $("button#activity").val();
+    var question3 = $("button#who").val();
+    var question4 = $("button#climate").val();
+    var question5 = $("button#distance").val();
+
+    var answer =  0;
+    if (question1 === "active" ) {
+      answer = + 1;
+    } else if {
+      answer = + 2;
+      } else {
+        answer = + 0;
+      }
+
+    if (question2 === "eat" ) {
+      answer = + 1;
+    } else if {
+      answer = + 2;
+      } else {
+        answer = + 0;
+      }
+
+    if (question3 === "friends" ) {
+      answer = + 1;
+    } else if {
+        answer = + 2;
+      } else {
+        answer = + 0;
+      }
+
+    if (question4 === "warm" ) {
+      answer = + 1;
+    } else if {
+      answer = + 2;
+      } else {
+        answer = + 0;
+      }
+
+    if (question5 === "close" ) {
+      answer = + 1;
+    } else if {
+      answer = + 2;
+      } else {
+        answer = + 0;
+      }
+
+    //results
+    var destination = ()
+
+  }
 });
