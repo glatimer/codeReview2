625baa20b81fc1402845ce2c5711567385ab6ecc
Genevieve Latimer <genevievelatimer@gmail.com>
2016-02-19 23:20:39 -0800

    WIP: Change q&a buttons to dropdown selectors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,63 @@
 $(document).ready(function() {
-  $("#questions").submit() {
+  $("#questions form").submit(function(event) {
     var question1 = $("button#goal").val();
     var question2 = $("button#activity").val();
     var question3 = $("button#who").val();
     var question4 = $("button#climate").val();
     var question5 = $("button#distance").val();
 
+
     var answer =  0;
     if (question1 === "active" ) {
       answer = + 1;
-    } else if {
+    } else if (question1 === "rest") {
       answer = + 2;
       } else {
         answer = + 0;
       }
-
     if (question2 === "eat" ) {
-      answer = + 1;
-    } else if {
-      answer = + 2;
+      answer = + 3;
+    } else if (question2 === "explore") {
+      answer = + 4;
       } else {
         answer = + 0;
       }
-
     if (question3 === "friends" ) {
-      answer = + 1;
-    } else if {
-        answer = + 2;
+      answer = + 5;
+    } else if (question3 === "family") {
+        answer = + 6;
       } else {
         answer = + 0;
       }
-
     if (question4 === "warm" ) {
-      answer = + 1;
-    } else if {
-      answer = + 2;
+      answer = + 7;
+    } else if (question4 === "cool") {
+      answer = + 8;
       } else {
         answer = + 0;
       }
+    if (question4 === "close" ) {
+      answer = + 9;
+    } else if (question4 === "far") {
+      answer = + 20;
+      } else {
+        answer = + 0;
+      }
+      event.preventDefault();
+    });
 
 
 
-    //results
-  (document).write("<p>"Okay, " + name + "." + " You are planning a trip including you and your " + question3 + "." + " The goal is to " + question1 + " and " + question2 + " in a " + question4 + " but " + question5 + " place" + "." + "Your ideal destination is" + destinationResult + "!"</p>");
-  }
+      var destinationResult = ["question1", "question2", "question3", "question4", "question5"]
+      if (answer <= 25){
+        $("span#summary").append(<"More information is needed.ß");
+      } else if (answer >=26 ) {
+        $("#nola").show();
+      } else if (answer >= 28) {
+        $("#denver").show();
+      } else if (answer >= 36) {
+        $("#barcelona").show();
+      } else if (answer >= 37) {
+        $("#copenhagen").show();
+      }
 });
