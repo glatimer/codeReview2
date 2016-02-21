1978256e55e46012c186e63c6fa26c0cec5e14de
Genevieve Latimer <genevievelatimer@gmail.com>
2016-02-21 14:42:33 -0800

    Add .toggle between question and result information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
- +  $("#questions form").submit(function(event) {
-      var question1 = $("select#goal").val();
-      var question2 = $("select#activity").val();
-      var question3 = $("select#who").val();
-      var question4 = $("select#climate").val();
-      var question5 = $("select#distance").val();
++  $("form#questions").submit(function(event) {
+    var question1 = $("select#goal").val();
+    var question2 = $("select#activity").val();
+    var question3 = $("select#who").val();
+    var question4 = $("select#climate").val();
+    var question5 = $("select#distance").val();
 
-      var result = "." + question1 + "." + question2 + "." + question3;
-
-    if ( question4 === "warm" ) {
+    var result = [question5, question4, question3, question2, question1];
+    if (result === "close" && "warm" || "friends" || "eat" ||"rest") {
       $(".summary#nola").show();
-    } else if ( question4 === "cool" ){
+    } else if (result === "far" && "warm" || "friends" || "eat" || "rest") {
       $(".summary#barcelona").show();
-    }
-
-    if ( question5  === "close" ) {
+    } else if (result  === "close" && "cool" || "family" || "explore" || "active") {
       $(".summary#denver").show();
-    } else if ( question5 === "far" ) {
+    } else if (result  === "far" && "cool" || "family" || "explore" || "active") {
       $("#copenhagen").show();
+    } else {
+      $(document).write("<h2>More information is needed to find out your personalized vacation!</h2>");
     }
 
     $("#first").toggle();
-    $(result).toggle();
+    $("#summary").toggle();
 
 
     event.preventDefault();
