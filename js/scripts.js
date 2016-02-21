6ea3f9067e0f58da907bced18c43242f989894e1
Gavin Latimer <Gavin@Wallaces-iMac.local>
2016-02-20 17:00:25 -0800

    WIP: try to make submit function work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,29 @@
 $(document).ready(function() {
-  $("form#questions").submit(function(event) {
-    var question1 = $("select#goal").val();
-    var question2 = $("select#activity").val();
-    var question3 = $("select#who").val();
-    var question4 = $("select#climate").val();
-    var question5 = $("select#distance").val();
+ - $("#questions").submit(function() {
+ +  $("#questions form").submit(function(event) {
+      var question1 = $("select#goal").val();
+      var question2 = $("select#activity").val();
+      var question3 = $("select#who").val();
+      var question4 = $("select#climate").val();
+      var question5 = $("select#distance").val();
 
-    var result =  [question1 + question2 + question3 + question4 + question5];
+      var result = "." + question1 + "." + question2 + "." + question3;
 
-    if (result === ["rest" || "eat" || "friends" || "warm" || "close"] ) {
-      ("#nola").show();
-    } else if (result === ["active" || "explore" || "family" || "cool" || "close"]) {
-      ("#denver").show();
-    } else if (result === ["active" || "eat" || "friends" || "warm" || "far"]) {
-      ("#barcelona").show();
-    } else if (result === ["rest" || "explore" || "family" || "cool" || "far"]) {
-      ("#copenhagen").show();
+    if ( question4 === "warm" ) {
+      $(".summary#nola").show();
+    } else if ( question4 === "cool" ){
+      $(".summary#barcelona").show();
+    }
 
+    if ( question5  === "close" ) {
+      $(".summary#denver").show();
+    } else if ( question5 === "far" ) {
+      $("#copenhagen").show();
+    }
 
+    $("#first").toggle();
+    $(result).toggle();
 
     event.preventDefault();
-
-
   });
 });
-
-
-var answer =  0;
-if (question1 === "active" ) {
-  answer = + 1;
-} else if (question1 === "rest") {
-  answer = + 2;
-  } else {
-    answer = + 0;
-  }
-if (question2 === "eat" ) {
-  answer = + 3;
-} else if (question2 === "explore") {
-  answer = + 4;
-  } else {
-    answer = + 0;
-  }
-if (question3 === "friends" ) {
-  answer = + 5;
-} else if (question3 === "family") {
-    answer = + 6;
-  } else {
-    answer = + 0;
-  }
-if (question4 === "warm" ) {
-  answer = + 7;
-} else if (question4 === "cool") {
-  answer = + 8;
-  } else {
-    answer = + 0;
-  }
-if (question4 === "close" ) {
-  answer = + 9;
-} else if (question4 === "far") {
-  answer = + 20;
-  } else {
-    answer = + 0;
-  }
