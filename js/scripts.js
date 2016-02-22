7a232f2873b7802e9c3bdc6d9fabbefd7aa40dce
Genevieve Latimer <genevievelatimer@gmail.com>
2016-02-21 16:31:13 -0800

    Attempt 2nd way of making conditions produce logical answers.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,24 @@ $(document).ready(function() {
     var question4 = $("select#climate").val();
     var question5 = $("select#distance").val();
 
-    var result = (question5, question4, question3, question2, question1);
-    if (result === "close" && "warm" && "friends" && "eat" && "rest") {
-      $("#nola").show();
-    } else if (result === "far" && "warm" && "friends" && "eat" && "rest") {
-      $("#barcelona").show();
-    } else if (result  === "close" && "cool" && "family" && "explore" && "active") {
-      $("#denver").show();
-    } else if (result  === "far" && "cool" && "family" && "explore" && "active") {
-      $("#copenhagen").show();
+
+
+    var climateDistance = (question4, question5);
+    var result = [];
+    if (climateDistance === "warm" && "close") {
+      result = ["#nola"];
+    } else if (climateDistance === "warm" && "far") {
+      result = ["#barcelona"];
+    } else if (climateDistance === "cool" && "close") {
+      result = ["#denver"];
+    } else if (climateDistance === "cool" && "far") {
+      result = ["#copenhagen"];
     } else {
       $("#summary").append("<strong><h2>More information is needed to find your dream vacation!</h2></strong>");
     }
+
+
+
 
     $("#first").toggle();
     $(result).toggle();
